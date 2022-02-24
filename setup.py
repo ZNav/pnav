67f1c4a7ae4138aa781d33801ab5f5de3a745e3c
Zander Navratil <50341426+ZNav@users.noreply.github.com>
2022-02-24 17:05:46 -0500

    Add files via upload

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ import setuptools
 
 from pathlib import Path
 this_directory = Path(__file__).parent
-long_description = (this_directory / "readme.txt").read_text()
+long_description = (this_directory / "README.md").read_text()
 
 setuptools.setup(
     name='pnav',
-    version='0.2.4',
+    version='0.2.6',
     author='Zander Navratil',
     description="Package with random functions, some are useful, some aren't so much. They are however used often, maintained, and good for learning.",
     packages=['pnav'],
     long_description=long_description,
-    long_description_content_type='text/markdown')
+    long_description_content_type='text/markdown',
+    url='https://pypi.org/project/pnav/')
